b90d1d398fdab27313b6f00c3cd58da4f4c278a8
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-16 02:07:43 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+// Your code 
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#000000';
+dodger.style.backgroundColor = '#FF69B4';
+dodger.style.bottom = '100px';
+dodger.style.bottom = '0px';
+
+
+
+function moveDodgerLeft() {
+    dodger.style.left = '0px';
+    dodger.style.marginBottom = '200px';
+}
+moveDodgerLeft()
+
+
+function moveDodgerRight() {
+    dodger.style.left = '360px'
+    dodger.style.marginBottom = '200px';
+}
+moveDodgerRight()
+
